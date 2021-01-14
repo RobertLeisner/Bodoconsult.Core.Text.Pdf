4192e1034d252a15e50da4198e190c26c1c9faf5
Robert Leisner <robert.leisner@bodoconsult.de>
2021-01-14 13:27:17 +0100

    first commit

---
+++ Bodoconsult.Core.Text.Pdf/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bodoconsult.Core.Text.Pdf")]
+[assembly: AssemblyDescription("Enhances Bodoconsult.Core.Text, a simple class for collecting structured text in an app, with an export to PDF")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bodoconsult EDV-Dienstleistungen GmbH")]
+[assembly: AssemblyProduct("Bodoconsult.Core.Text.Pdf")]
+[assembly: AssemblyCopyright("Copyright © Bodoconsult EDV-Dienstleistungen GmbH 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("11db26d5-44c9-47de-8195-9a70553857f7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
